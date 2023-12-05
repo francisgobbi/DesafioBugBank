e29ba1df66c45ec29703e026a944d7002e5a891e
francisgobbi <francis.gobbi@db.tec.br>
2023-12-05 09:03:08 -0300

    update positive tests

--- src/test/java/TestCases/PositiveLoginTest.java
+++ src/test/java/TestCases/PositiveLoginTest.java
@@ -1,7 +1,10 @@
 package TestCases;
 
 import Framework.TestBase;
+import PageObjects.CadastroPage;
+import PageObjects.LoginPage;
 import Tasks.TaskCriarContasTransferenciaDeValorEntreContas;
+import Validations.BemVindoValidations;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 
@@ -11,9 +14,31 @@ public class PositiveLoginTest  extends TestBase {
 
     private WebDriver driver = getDriverManager();
 
-    TaskCriarContasTransferenciaDeValorEntreContas transferencia = new TaskCriarContasTransferenciaDeValorEntreContas(driver);
+    String emailFer = "fernanda@automatizado.com";
+    String senhaFer = "1234";
+    CadastroPage cadastroPage = new CadastroPage(driver);
+    BemVindoValidations bemVindoValidations = new BemVindoValidations(driver);
+
+    LoginPage loginPage = new LoginPage(driver);
     @Test
     public void PositiveLogin() throws IOException {
-        transferencia.realizarLogin();
+        cadastroPage.getBotaoRegistrar().click();
+        cadastroPage.getEmail().clear();
+        cadastroPage.getEmail().sendKeys(emailFer);
+        cadastroPage.getNome().clear();
+        cadastroPage.getNome().sendKeys("Fernanda");
+        cadastroPage.getSenha().clear();
+        cadastroPage.getSenha().sendKeys(senhaFer);
+        cadastroPage.getConfirmaSenha().clear();
+        cadastroPage.getConfirmaSenha().sendKeys(senhaFer);
+        cadastroPage.getCriarContaComSaldo().click();
+        cadastroPage.getBotaoCadastrar().click();
+        cadastroPage.getBotaoFechar().click();
+        loginPage.getEmailLogin().sendKeys(emailFer);
+        loginPage.getSenhaLogin().sendKeys(senhaFer);
+        loginPage.getBotaoAcessar().click();
+        loginPage.getTextoBemVindo().getText();
+
+        bemVindoValidations.BemVindoValidationsOK();
     }
 }
